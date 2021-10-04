41d445f91c843c8528f6d386f6aa083e1bf50c0c
Duke of Sto Helit <myles.r.macdonald@gmail.com>
2021-10-04 13:15:35 -0500

    Monday 1PM

--- data/App_Trader_Scripts.sql
+++ data/App_Trader_Scripts.sql
@@ -89,28 +89,27 @@
 
 
 
--- WITH main_query AS (SELECT DISTINCT(aa.name) AS app_name, ROUND(((((ROUND(ROUND(aa.rating/5, 1) *5, 1) *2) +1) + ((ROUND(ROUND(pa.rating/5, 1) *5, 1) * 2) +1)) / 2), 2) AS avg_lifespan, 
--- 				ROUND(((((ROUND(ROUND(aa.rating/5, 1)*5, 1) *2) +1) + ((ROUND(ROUND(pa.rating/5, 1)*5, 1) * 2) +1)) / 2) * 12 * 5000, 2) AS expected_revenue, 
--- 				ROUND(((((ROUND(ROUND(aa.rating/5, 1)*5, 1) *2) +1) + ((ROUND(ROUND(pa.rating/5, 1)*5, 1) * 2) +1)) / 2) * 12 * 1000, 2) AS marketing_costs, 
--- 				aa.price AS apple_price, TRIM(REPLACE(pa.price, '$', '')) AS play_price, 
--- 				aa.primary_genre AS apple_genre, pa.genres AS play_genre, aa.content_rating AS acontent_rating, aa.review_count AS areview_count,				
--- 					CASE WHEN aa.price <= 1 THEN 10000.00
--- 					WHEN aa.price > 1 THEN aa.price * 10000 END AS app_purchase_price,
--- 					CASE WHEN TRIM(REPLACE(pa.price, '$', ''))::numeric <= 1 THEN 10000.00
--- 					WHEN TRIM(REPLACE(pa.price, '$', ''))::numeric > 1 THEN TRIM(REPLACE(pa.price, '$', ''))::numeric * 10000 END AS play_purchase_price
--- 				FROM play_store_apps AS pa INNER JOIN app_store_apps AS aa
--- 				ON aa.name = pa.name
--- 				ORDER BY expected_revenue DESC),
--- 	play_review AS (SELECT name, MAX(review_count) AS preview_count FROM play_store_apps
--- 				GROUP BY name
--- 				ORDER BY review_count)
--- SELECT * 
--- FROM play_review -- 
--- LEFT JOIN app_name, expected_revenue - marketing_costs - GREATEST(app_purchase_price, play_purchase_price) AS net_profit, apple_genre, play_genre, review_count, acontent_rating,  preview_count
--- -- FROM main_query
--- -- ORDER BY net_profit DESC, review_count::numeric DESC
+WITH main_query AS (SELECT DISTINCT(aa.name) AS app_name, ROUND(((((ROUND(ROUND(aa.rating/5, 1) *5, 1) *2) +1) + ((ROUND(ROUND(pa.rating/5, 1) *5, 1) * 2) +1)) / 2), 2) AS avg_lifespan, 
+				ROUND(((((ROUND(ROUND(aa.rating/5, 1)*5, 1) *2) +1) + ((ROUND(ROUND(pa.rating/5, 1)*5, 1) * 2) +1)) / 2) * 12 * 5000, 2) AS expected_revenue, 
+				ROUND(((((ROUND(ROUND(aa.rating/5, 1)*5, 1) *2) +1) + ((ROUND(ROUND(pa.rating/5, 1)*5, 1) * 2) +1)) / 2) * 12 * 1000, 2) AS marketing_costs, 
+				aa.price AS apple_price, TRIM(REPLACE(pa.price, '$', '')) AS play_price, 
+				aa.primary_genre AS apple_genre, pa.genres AS play_genre, aa.content_rating AS acontent_rating, aa.review_count AS areview_count,				
+					CASE WHEN aa.price <= 1 THEN 10000.00
+					WHEN aa.price > 1 THEN aa.price * 10000 END AS app_purchase_price,
+					CASE WHEN TRIM(REPLACE(pa.price, '$', ''))::numeric <= 1 THEN 10000.00
+					WHEN TRIM(REPLACE(pa.price, '$', ''))::numeric > 1 THEN TRIM(REPLACE(pa.price, '$', ''))::numeric * 10000 END AS play_purchase_price
+				FROM play_store_apps AS pa INNER JOIN app_store_apps AS aa
+				ON aa.name = pa.name
+				ORDER BY expected_revenue DESC),
+	play_review AS (SELECT name, MAX(review_count) AS preview_count 
+				FROM play_store_apps AS pa
+				GROUP BY name
+				ORDER BY preview_count)
+SELECT app_name, expected_revenue - marketing_costs - GREATEST(app_purchase_price, play_purchase_price) AS net_profit, apple_genre, play_genre, areview_count::numeric + preview_count::numeric AS total_reviews, acontent_rating 
+FROM main_query
+LEFT JOIN play_review
+ON app_name = name
+ORDER BY net_profit DESC, areview_count::numeric + preview_count::numeric DESC
 
--- SELECT name, MAX(review_count) AS review_count
--- 
 
 
